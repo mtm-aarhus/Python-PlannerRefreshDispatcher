eb7fce3de2e9a04be93fb0d53db64306666c57e4
Jakob Duus Terkelsen <jakobduus@gmail.com>
2025-01-30 15:09:01 +0100

    revert commit

--- __main__.py
+++
@@ -1,7 +0,0 @@
-"""The entry point of the process."""
-
-from robot_framework import linear_framework
-linear_framework.main()
-
-# from robot_framework import queue_framework
-# queue_framework.main()
